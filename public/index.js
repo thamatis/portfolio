ed93969ddb55311bc7ad662f527e5aff054107f8
einzberne <einzberne@gmail.com>
2025-01-13 21:34:03 +0700

    init

---
+++ public/index.js
@@ -0,0 +1,99 @@
+"use strict";
+
+function qs(selector, all = false) {
+  return all
+    ? document.querySelectorAll(selector)
+    : document.querySelector(selector);
+}
+
+function scrollHandler(e) {
+  const sections = qs(".section", true);
+  const timeline = qs(".timeline");
+  const line = qs(".line");
+  line.style.bottom = `calc(100% - 20px)`;
+  let prevScrollY = window.scrollY;
+  let up, down;
+  let full = false;
+  let set = 0;
+  const targetY = window.innerHeight * 0.8;
+
+  const { scrollY } = window;
+  up = scrollY < prevScrollY;
+  down = !up;
+  const timelineRect = timeline.getBoundingClientRect();
+  const lineRect = line.getBoundingClientRect(); // const lineHeight = lineRect.bottom - lineRect.top;
+
+  const dist = targetY - timelineRect.top;
+  console.log(dist);
+
+  if (down && !full) {
+    set = Math.max(set, dist);
+    line.style.bottom = `calc(100% - ${set}px)`;
+  }
+
+  if (dist > timeline.offsetHeight + 50 && !full) {
+    full = true;
+    line.style.bottom = `-50px`;
+  }
+
+  sections.forEach((item) => {
+    // console.log(item);
+    const rect = item.getBoundingClientRect(); //     console.log(rect);
+
+    if (rect.top + item.offsetHeight / 5 < targetY) {
+      item.classList.add("show-me");
+    }
+  }); // console.log(up, down);
+
+  prevScrollY = window.scrollY;
+  line.style.display = "block";
+}
+
+scrollHandler();
+window.addEventListener("scroll", scrollHandler);
+
+function scrollHandler2(e) {
+  const sections = qs(".educationContainer .section", true);
+  const timeline = qs(".educationContainer .timeline");
+  const line = qs(".educationContainer .line");
+  line.style.bottom = `calc(100% - 20px)`;
+  let prevScrollY = window.scrollY;
+  let up, down;
+  let full = false;
+  let set = 0;
+  const targetY = window.innerHeight * 0.8;
+
+  const { scrollY } = window;
+  up = scrollY < prevScrollY;
+  down = !up;
+  const timelineRect = timeline.getBoundingClientRect();
+  const lineRect = line.getBoundingClientRect(); // const lineHeight = lineRect.bottom - lineRect.top;
+
+  const dist = targetY - timelineRect.top;
+  console.log(dist);
+
+  if (down && !full) {
+    set = Math.max(set, dist);
+    line.style.bottom = `calc(100% - ${set}px)`;
+  }
+
+  if (dist > timeline.offsetHeight + 50 && !full) {
+    full = true;
+    line.style.bottom = `-50px`;
+  }
+
+  sections.forEach((item) => {
+    // console.log(item);
+    const rect = item.getBoundingClientRect(); //     console.log(rect);
+
+    if (rect.top + item.offsetHeight / 5 < targetY) {
+      item.classList.add("show-me");
+    }
+  }); // console.log(up, down);
+
+  prevScrollY = window.scrollY;
+  line.style.display = "block";
+}
+
+scrollHandler2();
+window.addEventListener("scroll", scrollHandler2);
